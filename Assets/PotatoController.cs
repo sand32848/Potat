331c90133fa298a9a874fcb989fc00e5f8aab9fe
sand32848 <54356212+sand32848@users.noreply.github.com>
2023-02-04 21:57:53 +0700

    Update potato rigidbody

--- Assets/PotatoController.cs
+++ Assets/PotatoController.cs
@@ -17,7 +17,8 @@ public class PotatoController : MonoBehaviour
     private GameObject gimball => GameObject.FindGameObjectWithTag("Gimball");
 
     private GameObject cam => Camera.main.gameObject;
-
+    RaycastHit slopeHit;
+    Vector3 slopeMoveDirection;
 
     private void Start()
     {
@@ -27,15 +28,24 @@ public class PotatoController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector3 targetDirection = new Vector3(ballTracker.transform.position.x, transform.position.y, ballTracker.transform.position.z) - rb.transform.position;
+        slopeMoveDirection = Vector3.ProjectOnPlane(gimball.transform.rotation.eulerAngles.normalized, slopeHit.normal);
 
-        // Rotate the forward vector towards the target direction by one step
-        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, camLookSpeed, 0.0f);
+        print(rb.velocity.normalized);
 
-        // Draw a ray pointing at our target in
-        Debug.DrawRay(transform.position, newDirection, Color.red);
+        if (onSlope())
+        {
+    
+            rb.AddForce(slopeMoveDirection * InputController.Instance.movement.y * moveSpeed * 2f);
+  
+            Debug.DrawRay(transform.position, slopeMoveDirection * 5, Color.red);
+            print(slopeMoveDirection);
+        }
+        else
+        {
+            rb.AddForce(cam.transform.forward * InputController.Instance.movement.y * moveSpeed);
+        }
 
-        rb.AddForce(cam.transform.forward * InputController.Instance.movement.y * moveSpeed);
+       // print(onSlope());
 
         if (InputController.Instance.movement.x == 1)
         {
@@ -46,13 +56,29 @@ public class PotatoController : MonoBehaviour
             rb.AddForceAtPosition(Vector3.up * -flipSpeed, posLeft.transform.position);
         }
 
-
-
         if (InputController.Instance.movement.y != 0)
         {
             transform.rotation = Quaternion.Lerp(transform.rotation, gimball.transform.rotation, Time.deltaTime * camLookSpeed);
-        }   
+        }
 
-
+ 
     }
+
+    public bool onSlope()
+    {
+        if(Physics.Raycast(transform.position + new Vector3(0,0,0.01f),Vector3.down,out slopeHit, 4))
+        {
+            if (slopeHit.normal != Vector3.up)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return false;
+    }
+
+  
 }
